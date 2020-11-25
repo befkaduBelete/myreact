e333619b43b8df3d9605e791af0adbe24ced85ff
befkaduBelete <befkadu.beletee@gmail.com>
2020-11-25 22:33:43 +0300

    This is some chnage

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import Radium from 'radium';
 
 import './Person.css';
 
+const style ={
+    '@media (min-width:500px)':{
+        width:'400px'
+    }
+};
+
 const person = ( props ) => {
     return (
-        <div className="Person">
-            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
+        <div className="Person" style={style}>
+            <p onClick={props.click}>I'm mmm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />
         </div>
     )
 };
 
-export default person;
+export default Radium(person);
